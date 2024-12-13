fd6b837a469dcb10ae41623336809ff30489bc75
Renuka Ajay Chougule <renukachougule.0523@gmail.com>
2024-12-13 15:45:27 +0530

    Add project files.

---
+++ SummingElement/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SummingElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40 };
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
